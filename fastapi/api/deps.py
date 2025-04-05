48bdbd890935210eb68a61402e4226b433f9d597
Jose Miguel Puiggros Tejada <josepuiggrost@gmail.com>
2025-04-05 16:16:59 -0300

    Revert "Api+db"

---
+++ fastapi/api/deps.py
@@ -0,0 +1,41 @@
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+import os
+from .database import SessionLocal
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv("AUTH_SECRET_KEY")
+ALGOTITHM = os.getenv("AUTH_ALGORITHM")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
+oauth2_bearer_dependency = Annotated[str, Depends(oauth2_bearer)]
+
+
+async def get_current_user(token: oauth2_bearer_dependency):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGOTITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user")
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user")
+    
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
